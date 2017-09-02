d61477a5aff18f5b34a8ed3743f721b3fda3e69a
jaw653 <jawax10@gmail.com>
2017-09-01 21:37:11 -0500

    i believe i got the first remove working

--- cda.c
+++ cda.c
@@ -5,6 +5,9 @@
  *circular dynamic array object
  *
  *Questions:
+ *-for removeCDA, should we shrink before or after the element is removed
+ *-line 117(ish) if the array is empty, should i just return null?
+ *-does removeCDA return the value that was removed?
  *-make sure that you added in all assertions
  *-can i add my own "private" helper functions into these classes?
  */
@@ -74,7 +77,7 @@ void insertCDAfront(CDA *items, void *value) {
     }
   }
 }
-/************* need to find all possible input cases for this and previous insert ***********/
+
 void insertCDAback(DA *items, void *value) {
   //If array is empty
   if (items->filledIndices == 0) {
@@ -84,7 +87,7 @@ void insertCDAback(DA *items, void *value) {
   else {
     if (items->filledIndices == items->size) {
       if (items->frontIndex != 0) {
-        assert( items->size * 2 * sizeof(void*) );
+        assert( items->size * 2 * sizeof(void*) != 0);
         items->array = realloc( items->array, 2 * items->size * sizeof(void*) );
         items->size *= 2;
 
@@ -112,7 +115,58 @@ void insertCDAback(DA *items, void *value) {
 }
 
 void *removeCDAfront(CDA *items) {
+  if (items->filledIndices == 0) {
+    return NULL;
+  }
+  else {
+    void *valToReturn = items->array[items->frontIndex];
+    items->array[items->frontIndex] = NULL;
+    items->filledIndices -= 1;
 
+    if (items->frontIndex + 1 == size) { items->frontIndex = 0; }
+    else { items->frontIndex += 1; }
+
+    if (items->filledIndices < .25 * items->size) {
+      /*
+       *The following code creates a new tmpArray and fills it with all the non NULL
+       *values in the original array. It then cuts the size of the items->array (the
+       *actual object) by /2. It then repopulates the resized array with tmpArray, so
+       *every resized array will have a frontIndex of 0 and a backIndex of filledIndices
+       *minus one. Removing the requested value happens after this "if" code block.
+       */
+      assert( (items->size / 2) * sizeof(void*) != 0 );
+
+      void **tmpArr = malloc( items->filledIndices * sizeof(void*) );
+      void *ptr = items->array[items->frontIndex];
+
+      int index = items->frontIndex;
+      int tmpIndex = 0;
+      while (ptr) {
+        tmpArr[tmpIndex] = ptr;
+        index += 1;
+
+        if (index == items->size) {
+          index = 0;
+        }
+
+        ptr = items->array[index];
+        tmpIndex += 1;
+      }
+
+      items->array = realloc( items->array, (items->size / 2) * sizeof(void*) );
+      items->size /= 2;
+
+      int i;
+      for (i = 0; i < items->filledIndices; i++) {
+        items->array[i] = tmpArr[i];
+      }
+
+      items->frontIndex = 0;
+      items->backIndex = items->filledIndices - 1;
+    }
+  }
+
+  return valToReturn;
 }
 void *removeCDAback(CDA *items);
 void unionCDA(CDA *recipient,CDA *donor);
